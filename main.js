461910dce256ef6ee42415607c1c93bc0aa1d890
DaffaRadh <daffasven@gmail.com>
2022-09-28 20:45:15 +0700

    give js

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#multiText', {
+	strings: ['Frontend Developer.', 'UI Designer.', 'Student.'],
+	typeSpeed: 60,
+	backSpeed:60,
+	loop: true,
+	loopCount: Infinity,
+	startDelay:500,
+	
+});
